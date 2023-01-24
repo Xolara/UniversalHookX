70900edfbdffc721add4203e01ee27decd740c37
Wesley B Xolara <dawgyodawg@gmail.com>
2023-01-23 22:00:45 -0400

    Update menu.cpp

--- UniversalHookX/src/menu/menu.cpp
+++ UniversalHookX/src/menu/menu.cpp
@@ -5,6 +5,19 @@
 
 namespace ig = ImGui;
 
+float test = 1;
+
+static bool no_titlebar = false;
+static bool no_border = true;
+static bool no_resize = false;
+static bool auto_resize = false;
+static bool no_move = false;
+static bool no_scrollbar = false;
+static bool no_collapse = false;
+static bool no_menu = true;
+static bool start_pos_set = false;
+
+
 namespace Menu {
     void InitializeContext(HWND hwnd) {
         if (ig::GetCurrentContext( ))
@@ -21,6 +34,26 @@ namespace Menu {
         if (!bShowMenu)
             return;
 
-        ig::ShowDemoWindow( );
+        ImGui::Begin("ImGui Menu", &bShowMenu);
+
+        ImGui::PushItemWidth(ImGui::GetWindowWidth( ) * 0.65f);
+        ImGui::PushItemWidth(-140);
+
+        if (ImGui::CollapsingHeader("MENU")) {
+            if (ImGui::TreeNode("SUB MENU")) {
+                ImGui::Text("Text Test");
+                if (ImGui::Button("Button Test")) {
+                }
+                ImGui::Checkbox("CheckBox Test", &no_titlebar);
+                ImGui::SliderFloat("Slider Test", &test, 1.0f, 100.0f);
+
+                ImGui::TreePop( );
+            }
+        }
+
+        ImGui::End( );
+        //ig::ShowDemoWindow( );
+        ImGui::Render( );
     }
+
 } // namespace Menu
